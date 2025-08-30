d2e20eb25889195fe4eda2213e2f7d58934b5378
unknown <nagilapereira8896@digitalcollege.internal>
2025-08-30 12:01:20 -0300

    tarefa de casa

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const contador = document.getElementById('count');
+const decrementar = document.getElementById('decrement');
+const resetar = document.getElementById('reset');
+const incrementar = document.getElementById('increment'); 
+
+let numero = 0;
+
+contador.innerText = numero;
+
+decrementar.addEventListener('click', ( => {
+    numero = numero - 1;
+    contador.innerText = numero;
+}));
